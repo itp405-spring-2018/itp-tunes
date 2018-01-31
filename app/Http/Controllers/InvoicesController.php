99ed6e3c403d3463ed974666e79c443c2ddb5926
David Tang <dtang859@gmail.com>
2018-01-30 19:41:04 -0800

    finished invoices and invoice-details section

---
+++ app/Http/Controllers/InvoicesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class InvoicesController extends Controller
+{
+  public function index()
+  {
+    $invoices = DB::table('invoices')
+      ->join('customers', 'invoices.CustomerId', '=', 'customers.CustomerId')
+      ->orderBy('InvoiceDate', 'desc')
+      ->limit(10)
+      ->get();
+    // dd($invoices);
+    return view('invoices', [
+      'invoices' => $invoices
+    ]);
+  }
+
+  public function show($invoiceID)
+  {
+    $invoiceItems = DB::table('invoice_items')
+      ->select('Quantity', 'invoice_items.UnitPrice', 'artists.Name as ArtistName', 'tracks.Name as TrackName')
+      ->join('tracks', 'invoice_items.TrackId', '=', 'tracks.TrackId')
+      ->join('albums', 'tracks.AlbumId', '=', 'albums.AlbumId')
+      ->join('artists', 'artists.ArtistId', '=', 'albums.ArtistId')
+      ->where('InvoiceId', '=', $invoiceID)
+      ->get();
+
+    return view('invoice-details', [
+      'invoiceItems' => $invoiceItems
+    ]);
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/invoices', 'InvoicesController@index');
+Route::get('/invoices/{id}', 'InvoicesController@show');
