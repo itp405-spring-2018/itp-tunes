ac79108a3509936116a73c1c01c96a39c0416ad1
David Tang <dtang859@gmail.com>
2018-02-06 19:35:13 -0800

    add a playlist page with validation

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,10 @@ Route::get('/', function () {
 Route::get('/invoices', 'InvoicesController@index');
 Route::get('/invoices/{id}', 'InvoicesController@show');
 Route::get('/playlists', 'PlaylistsController@index');
+Route::get('/playlists/new', 'PlaylistsController@create');
 Route::get('/playlists/{id}', 'PlaylistsController@show');
+Route::post('/playlists', 'PlaylistsController@store');
+
 
 Route::get('/phpinfo', function() {
   echo phpinfo();
