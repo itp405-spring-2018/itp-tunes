53b8607d845b054f6ad68754a416cd143a2c4f48
David Tang <dtang859@gmail.com>
2018-02-06 18:33:44 -0800

    Display a list of playlists and a single playlist

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/invoices', 'InvoicesController@index');
 Route::get('/invoices/{id}', 'InvoicesController@show');
+Route::get('/playlists', 'PlaylistsController@index');
+Route::get('/playlists/{id}', 'PlaylistsController@show');
 
 Route::get('/phpinfo', function() {
   echo phpinfo();
