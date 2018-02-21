c3bfee569c0f7aede068edf63f21c7ae50cdaa1b
David Tang <dtang859@gmail.com>
2018-02-20 19:57:43 -0800

    done

---
+++ app/Http/Controllers/SignupController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Hash;
+
+class SignupController extends Controller
+{
+    public function index()
+    {
+        return view('signup');
+    }
+
+    public function signup()
+    {
+        $user = new User();
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = Hash::make(request('password')); // bcrypt
+        $user->save();
+
+        return redirect('/login');
+    }
+}
