315c8ee25ded7c34ad1952c8b8c8124c8e43f7fa
David Tang <dtang859@gmail.com>
2018-04-03 09:51:11 -0700

    cleanup

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -28,10 +28,11 @@ class LoginController extends Controller
             $user = new User();
             $user->name = $twitterUser->getName();
             $user->email = $twitterUser->getEmail();
-            $user->twitter_token = $twitterUser->token;
-            $user->twitter_token_secret = $twitterUser->tokenSecret;
-            $user->save();
         }
+
+        $user->twitter_token = $twitterUser->token;
+        $user->twitter_token_secret = $twitterUser->tokenSecret;
+        $user->save();
 
         Auth::login($user);
         return redirect('/profile');
--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Auth;
 use Socialite;
 use App\Api\Twitter;
 
 class TweetsController extends Controller
 {
     public function store(Request $request)
-	{
-		$user = Auth::user();
-
-		$twitter = new Twitter();
-		$twitter->postStatus($request->body);
-		return redirect('/profile');
-	}
+    {
+        $twitter = new Twitter();
+        $twitter->postStatus($request->body);
+        return redirect('/profile');
+    }
 }
